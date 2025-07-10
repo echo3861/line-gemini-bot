e7f6e13521d552bf43ac6b4a4e26bffb9071fca4
echo3861 <adenw2020@gmail.com>
2025-07-10 20:49:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-pro-latest",
+    model_name="gemma-3n-e2b-it",
     generation_config={
         "temperature": 0.7,
         "max_output_tokens": 1024,
